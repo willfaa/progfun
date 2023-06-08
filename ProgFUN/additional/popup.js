627adeb6738a99f16b139089bcfbf9b30ea7ef54
willfaa <farywildanyfahmi@gmail.com>
2023-06-08 20:23:07 +0700

    Revert "progfun"
    This reverts commit 6a995f210e3eda24c3da520a927b32fb8d8f77f3.

--- ProgFUN/additional/popup.js
+++
@@ -1,6 +0,0 @@
-// function toggle() {
-//   var blur = document.getElementById('main1');
-//   blur.classList.toggle('active');
-//   var popup = document.getElementById('popup');
-//   popup.classList.toggle('active');
-// }
